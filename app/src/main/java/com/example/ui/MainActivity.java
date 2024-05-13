145c04311dd2cf556629c26f4220e1864839b633
minhphuc2544 <22521116@ms.uit.edu.vn>
2024-05-13 15:29:03 +0700

    Update custom adapter of notication's UI and rename all picture in drawable

--- app/src/main/java/com/example/ui/MainActivity.java
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recovery_password);
+        setContentView(R.layout.notification);
+
+
+        ListView lvPost = (ListView) findViewById(R.id.lvPost);
+        ArrayList<Notification> names = new ArrayList<Notification>();
+
+        names.add(new Notification("xzczxc", "121212", "2323"));
+        names.add(new Notification("xzczxc", "121212", "2323"));
+        names.add(new Notification("xzczxc", "121212", "2323"));
+        names.add(new Notification("xzczxc", "121212", "2323"));
+        names.add(new Notification("xzczxc", "121212", "2323"));
+        names.add(new Notification("xzczxc", "121212", "2323"));
+        names.add(new Notification("xzczxc", "121212", "2323"));
+
+        NotificationAdapter adapter = new NotificationAdapter(this, R.layout.notification_item, names);
+        lvPost.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/ui/Notification.java
@@ -0,0 +1,31 @@
+package com.example.ui;
+
+public class Notification {
+
+    private String image;
+    private String notiTitle;
+    private String notiDes;
+
+    public Notification(String a, String b, String c)
+    {
+        image = a;
+        notiTitle = b;
+        notiDes = c;
+    }
+
+    public Notification()
+    {
+        image = notiTitle = notiDes = "";
+    }
+
+    // get and set
+    public String getNotiTitle()
+    {
+        return notiTitle;
+    }
+
+    public String getNotiDes()
+    {
+        return notiDes;
+    }
+}
