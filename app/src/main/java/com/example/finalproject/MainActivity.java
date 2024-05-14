6113c5db0e8265814d823acc3d4f3bbacf643dc7
Vo Tran Phi <songokupkj@gmail.com>
2024-05-15 03:21:15 +0700

    chore: add retrofit dependency

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.http.HttpEngine;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        
+
                     }
                 }
         );
