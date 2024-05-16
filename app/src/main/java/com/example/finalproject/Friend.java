6c160e9c904b332ed5f704e305fdd10146164603
minhphuc2544 <22521116@ms.uit.edu.vn>
2024-05-16 10:15:53 +0700

    upload custom adapter for friend's UI

---
+++ app/src/main/java/com/example/finalproject/Friend.java
@@ -0,0 +1,26 @@
+package com.example.finalproject;
+
+public class Friend {
+    public String avatar;
+    public String username;
+
+    public Friend (String avatar, String username)
+    {
+        this.avatar = avatar;
+        this.username = username;
+    }
+    public Friend ()
+    {
+        avatar=username="";
+    }
+
+    //get and set
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/FriendAdapter.java
@@ -0,0 +1,35 @@
+package com.example.finalproject;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FriendAdapter extends ArrayAdapter<Friend> {
+    private final Activity context;
+    public FriendAdapter(Activity context, int layoutID, List<Friend> objects) {
+        super(context, layoutID, objects);
+        this.context = context;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.friend_item, null, false);
+        }
+        // Get item
+        Friend fr = getItem(position);
+        // Get view
+        ImageView profileImg = (ImageView) convertView.findViewById(R.id.profileImg);
+        TextView username = (TextView) convertView.findViewById(R.id.tvUsername);
+
+        // image
+        username.setText(fr.getUsername());
+        return convertView;
+    }
+}
