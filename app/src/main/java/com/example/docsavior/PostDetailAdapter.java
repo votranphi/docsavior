a3020455566591d1948947e50fc21180fb95cf88
minhphuc2544 <phucle190904@gmail.com>
2024-06-23 03:26:41 +0700

    feat: comment activity adapter and fix layout

--- app/src/main/java/com/example/docsavior/PostDetailAdapter.java
+++ app/src/main/java/com/example/docsavior/PostDetailAdapter.java
@@ -1,13 +1,26 @@
 package com.example.docsavior;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PostDetailAdapter extends ArrayAdapter<PostDetail> {
     private final Activity context;
@@ -26,12 +39,66 @@ public class PostDetailAdapter extends ArrayAdapter<PostDetail> {
         // Get view
         TextView username = (TextView) convertView.findViewById(R.id.tvUsername);
         TextView comment = (TextView) convertView.findViewById(R.id.tvComment);
-
+        ImageView profileImgs = (ImageView) convertView.findViewById(R.id.profileImg);
 
         username.setText(pd.getUsername());
+        getUserInfoAndSetAvatar(profileImgs, pd.getUsername());
         comment.setText(pd.getComment());
 
-
         return convertView;
+    }
+
+    private void getUserInfoAndSetAvatar(ImageView imageView, String username) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(ApplicationInfo.apiPath)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Call<User> call = apiService.getUserInfo(username);
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                try {
+                    if (response.isSuccessful()) {
+                        // after getting the user's info, set the avatar
+                        setImage(imageView, response.body().getAvatarData());
+                    } else {
+                        Toast.makeText(context, response.code() + response.errorBody().string(), Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception ex) {
+                    Log.e("ERROR106: ", ex.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(context, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void setImage(ImageView imageView, String avatarData) {
+        try {
+            if (!avatarData.isEmpty()) {
+                // create jsonArray to store avatarData
+                JSONArray jsonArray = new JSONArray(avatarData);
+
+                // convert jsonArray to byteArray
+                byte[] byteArray = new byte[jsonArray.length()];
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    int temp = (int)jsonArray.get(i);
+                    byteArray[i] = (byte)temp;
+                }
+
+                // convert byteArray to bitmap
+                Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                // set the avatar
+                imageView.setImageBitmap(Bitmap.createScaledBitmap(bmp, imageView.getWidth(), imageView.getHeight(), false));
+            }
+        } catch (Exception ex) {
+            Log.e("ERROR111: ", ex.getMessage());
+        }
     }
 }
