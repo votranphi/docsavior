9f94f5691ece64ff0fbff2042ecad032fa95ab07
minhphuc2544 <phucle190904@gmail.com>
2024-06-23 02:58:34 +0700

    fix: turn off show boolean type when loading newsfeed

--- app/src/main/java/com/example/docsavior/NewsFeedAdapter.java
+++ app/src/main/java/com/example/docsavior/NewsFeedAdapter.java
@@ -132,7 +132,6 @@ public class NewsFeedAdapter extends ArrayAdapter<NewsFeed> {
                         {
                             btnLikes.get(position).setImageResource(R.drawable.like_icon);
                             isLiked.set(position, true);
-                            Toast.makeText(context, isLiked.get(position).toString(), Toast.LENGTH_SHORT).show();
                             // add animation change from like to unlike
                         } else if (res.equals("dislike"))
                         {
@@ -197,7 +196,6 @@ public class NewsFeedAdapter extends ArrayAdapter<NewsFeed> {
             public void onClick(View v) {
 
                 Retrofit retrofit = new Retrofit.Builder().baseUrl(ApplicationInfo.apiPath).addConverterFactory(GsonConverterFactory.create()).build();
-
                 ApiService apiService = retrofit.create(ApiService.class);
                 if(isLiked.get(position)) // if already liked
                 {
