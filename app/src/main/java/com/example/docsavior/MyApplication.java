c0ed4d602c939bf05160031ee839a7da16753f42
minhphuc2544 <phucle190904@gmail.com>
2024-07-02 02:29:19 +0700

    feat: set default theme (only light theme)

---
+++ app/src/main/java/com/example/docsavior/MyApplication.java
@@ -0,0 +1,12 @@
+package com.example.docsavior;
+
+import android.app.Application;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+}
